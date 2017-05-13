f785eee89075be084598110379ded31dc4377edd
IgorDems <dems_i@yahoo.com>
2017-05-13 00:13:43 -0700

    Level3_HW5 NewHomework

---
+++ Level3_HW5/Louncher.java
@@ -0,0 +1,12 @@
+package ProfileFF;
+
+public class Louncher {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		Tests run = new Tests();
+		run.login();
+		run.logout();
+	}
+
+}
